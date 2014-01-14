43d28f96fae3d10eeb2873ff9d3a0607d2d43147
Your Name <kaspar93@msn.com>
2014-01-14 18:08:44 +0200

    My version!

---
+++ Assets/Example.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Example : MonoBehaviour {
+	void Update() {
+		SpriteRenderer spriteRenderer = gameObject.GetComponent<SpriteRenderer> ();
+		if (Spawner.getControlledCannon () == transform.parent.name) {
+						if (spriteRenderer.color != Color.white)
+								spriteRenderer.color = Color.white;
+				}
+		else
+			if (spriteRenderer.color != Color.grey)
+						spriteRenderer.color = Color.grey;
+	}
+}
--- Assets/Spawner.cs
+++ Assets/Spawner.cs
@@ -40,40 +40,27 @@ public class Spawner : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-				if (Input.GetMouseButtonDown (0) && Input.GetKey ("left ctrl")) {
-						Vector3 newPosition = Vector3.zero;
-						Vector3 mouse = Input.mousePosition;
-						mouse.z = 1000;
-						newPosition = Camera.main.ScreenToWorldPoint (mouse);
-						newPosition.z = 0;
-						for (int i=0; i<10; i++) {
-								if (Mathf.Abs (newPosition.x - slots [i].transform.position.x) < 50 &&
-				    			Mathf.Abs (newPosition.y - slots [i].transform.position.y) < 50) {
-										int isThereACannon = 0;
-										for (int j=0; j<cannonCount; j++) {
-												if (Mathf.Abs (cannons [j].transform.position.x - newPosition.x) < 50 &&
-						    						Mathf.Abs (cannons [j].transform.position.y-newPosition.y) < 50) {
-														isThereACannon = 1;
-														break;
-												}
-										}
-										if (isThereACannon == 0) {
-												cannons [cannonCount] = (GameObject)Instantiate
-													(cannon, slots[i].transform.position, slots[i].transform.rotation);
-												cannons [cannonCount].name = cannons [cannonCount].name + cannonCount;
-												cannonCount++;
-										} else {
-												for (int j=0; j<cannonCount; j++) {
-														if (Mathf.Abs (cannons [j].transform.position.x - newPosition.x) < 50 &&
-																Mathf.Abs (cannons [j].transform.position.y - newPosition.y) < 50) {
-																controlledCannon = cannons [j].name;
-																break;
-														}
-												}
-										}
-								}
-						}
+		if (Input.GetMouseButtonUp (0) && Input.GetKey ("left ctrl")) {
+			Vector3 newPosition = Vector3.zero;
+			Vector3 mouse = Input.mousePosition;
+			mouse.z = 1000;
+			newPosition = Camera.main.ScreenToWorldPoint (mouse);
+			newPosition.z = 0;
+			for (int i=0; i<10; i++) {
+				if (Mathf.Abs (newPosition.x - slots [i].transform.position.x) < 50 &&
+				    Mathf.Abs (newPosition.y - slots [i].transform.position.y) < 50) {
+					if(cannons[i]!=null){
+						controlledCannon = cannons [i].name;
+					}
+					else{
+						cannons [i] = (GameObject)Instantiate
+							(cannon, slots[i].transform.position, slots[i].transform.rotation);
+						cannons [i].name = cannons [i].name + i;
+					}
+					
 				}
-	
+			}
+		}
+		
 	}
 }
